f338ea0d126969dd90a3c6b0170ce0a265134bf1
Блинов Максим <tenzm@mail.ru>
2019-11-27 11:01:52 +0300

    added -h

--- main.go
+++ main.go
@@ -11,15 +11,28 @@ import (
 var (
 	d = flag.String("d", ".", "Directory to process")
 	a = flag.Bool("a", false, "Print all info")
+	h = flag.Bool("h", false, "Print storage info")
 )
 
 func hrSize(fsize int64) string {
-	return "0KB"
+	if fsize/1048576 > 0 {
+		if fsize%1048576 > 0 {
+			return strconv.Itoa(int(fsize/1048576)+1) + "MB"
+		}
+		return strconv.Itoa(int(fsize/1048576)) + "MB"
+	}
+	if fsize%1024 > 0 {
+		return strconv.Itoa(int(fsize/1024)+1) + "KB"
+	}
+	return strconv.Itoa(int(fsize/1024)) + "KB"
 }
 
 func printAll(file os.FileInfo) {
 	time := file.ModTime().Format("Jan 06 15:4")
 	fSize := strconv.Itoa(int(file.Size()))
+	if *h == true {
+		fSize = hrSize(file.Size())
+	}
 	fmt.Printf("%s %s %s \n", fSize, time, file.Name())
 }
 
