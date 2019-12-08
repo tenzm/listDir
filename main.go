703d3cf72ab8a5036e7bdc2af737937d58eab9e8
Maksim Blinov <tenzm@mail.ru>
2019-12-08 19:50:13 +0300

    added interface sort

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 	"strconv"
+	"sort"
 )
 
 var (
@@ -15,7 +16,7 @@ var (
 	msort = flag.Bool("sort", false, "Sort for modification date")
 )
 
-func sort(files []os.FileInfo) {
+/*func sort(files []os.FileInfo) {
 	for j := 0; j < len(files); j++ {
 		for i := 0; i < len(files)-j-1; i++ {
 			if files[i].ModTime().Before(files[i+1].ModTime()) {
@@ -23,6 +24,22 @@ func sort(files []os.FileInfo) {
 			}
 		}
 	}
+}*/
+
+type FilesTimeSort struct {
+	files []os.FileInfo
+}
+
+func (fts *FilesTimeSort) Len() int{
+	return len(fts.files)
+}
+
+func (fts *FilesTimeSort) Less(i, j int) bool{
+	return fts.files[i].ModTime().Before(fts.files[j].ModTime())
+}
+
+func (fts *FilesTimeSort) Swap(i, j int){
+	fts.files[i], fts.files[j] = fts.files[j], fts.files[i]
 }
 
 func hrSize(fsize int64) string {
@@ -50,8 +67,9 @@ func printAll(file os.FileInfo) {
 func main() {
 	flag.Parse()
 	files, _ := ioutil.ReadDir(*d)
+	sortedFiles :=FilesTimeSort{files}
 	if *msort == true {
-		sort(files)
+		sort.Sort(&sortedFiles)
 	}
 	for _, file := range files {
 		if *a {
